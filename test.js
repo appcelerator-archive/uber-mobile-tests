2acfa5bde22863cd1a9e225392126cc57ae5a803
Wilson Luu <wluu@appcelerator.com>
2017-02-23 15:29:52 -0800

    Added commander.

--- test.js
+++ test.js
@@ -1,8 +1,19 @@
 'use strict';
 
-const unit = require('./lib/unit_test/helper.js');
+const
+	program = require('commander'),
+	unit = require('./lib/unit_test/helper.js'),
+	VER = require('./package.json').version;
 
-unit.test('master', ['ios'], (err, results) => {
+program
+	.version(VER)
+	// TODO Allow choosing a URL or zipfile as SDK to install!
+	.option('-b, --branch [branchName]', 'Install a specific branch of the SDK to test with', 'master')
+	.option('-p, --platforms <platform1,platform2>', 'Run unit tests on the given platforms', /^(android(,ios)?)|(ios(,android)?)$/, 'android,ios')
+	.parse(process.argv);
+
+const platforms = program.platforms.split(',');
+unit.test(program.branch, platforms, (err, results) => {
 	if (err) {
 		console.error(err.toString());
 		process.exit(1);
@@ -30,23 +41,3 @@ unit.test('master', ['ios'], (err, results) => {
 		process.exit(1);
 	});
 });
-
-/*
-	NOTE:
-	- pulled from https://github.com/appcelerator/titanium-mobile-mocha-suite/blob/master/scripts/test.js#L474-L514
-	- this will contain the main loop to run the unit tests first then the appium tests
-	- es6 this file
-
-	var program = require('commander'),
-		packageJson = require('./package'),
-		platforms = [];
-
-	program
-		.version(packageJson.version)
-		// TODO Allow choosing a URL or zipfile as SDK to install!
-		.option('-b, --branch [branchName]', 'Install a specific branch of the SDK to test with', 'master')
-		.option('-p, --platforms <platform1,platform2>', 'Run unit tests on the given platforms', /^(android(,ios)?)|(ios(,android)?)$/, 'android,ios')
-		.parse(process.argv);
-
-	platforms = program.platforms.split(',');
-*/
