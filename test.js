4e491b62f15cb7e0f0d5be745c40a89b44398af1
Wilson Luu <wluu@appcelerator.com>
2017-02-23 15:01:16 -0800

    Replaced wrench with fs-extra, created a template for app.js, and added logic to copy unit tests over to the titanium app.

--- test.js
+++ test.js
@@ -1,5 +1,36 @@
 'use strict';
 
+const unit = require('./lib/unit_test/helper.js');
+
+unit.test('master', ['ios'], (err, results) => {
+	if (err) {
+		console.error(err.toString());
+		process.exit(1);
+		return;
+	}
+
+	let p = Promise.resolve();
+
+	platforms.forEach(platform => {
+		p = p.then(() => {
+			return new Promise((resolve, reject) => {
+				console.log();
+				console.log('=====================================');
+				console.log(platform.toUpperCase());
+				console.log('-------------------------------------');
+				unit.outputResults(results[platform].results, resolve);
+			});
+		});
+	});
+
+	p.then(() => {
+		process.exit(0);
+	}).catch(err => {
+		console.error(err.toString());
+		process.exit(1);
+	});
+});
+
 /*
 	NOTE:
 	- pulled from https://github.com/appcelerator/titanium-mobile-mocha-suite/blob/master/scripts/test.js#L474-L514
@@ -18,28 +49,4 @@
 		.parse(process.argv);
 
 	platforms = program.platforms.split(',');
-
-	test(program.branch, platforms, function(err, results) {
-		if (err) {
-			console.error(err.toString());
-			process.exit(1);
-			return;
-		}
-
-		async.eachSeries(platforms, function (platform, next) {
-			console.log();
-			console.log('=====================================');
-			console.log(platform.toUpperCase());
-			console.log('-------------------------------------');
-			outputResults(results[platform].results, next);
-		}, function (err) {
-			if (err) {
-				console.error(err.toString());
-				process.exit(1);
-				return;
-			}
-
-			process.exit(0);
-		});
-	});
 */
