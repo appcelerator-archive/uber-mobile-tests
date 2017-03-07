cc0edbcfe772647838f01464acb26c08d7f53831
Wilson Luu <wluu@appcelerator.com>
2017-03-06 17:13:19 -0800

    Finished appc_util.js to work with unit_test/helper.js.

--- test.js
+++ test.js
@@ -15,8 +15,8 @@ program
 
 const platforms = program.platforms.split(',');
 
-// run unit tests first
-let p = new Promise((resolve, reject) => {
+new Promise((resolve, reject) => {
+	// run unit tests first
 	unit.test(program.branch, platforms, (err, results) => {
 		if (err) {
 			reject(err);
@@ -24,10 +24,9 @@ let p = new Promise((resolve, reject) => {
 		}
 		resolve(results);
 	});
-});
-
-// print out results from unit tests
-p.then(unitTestResults => {
+})
+.then(unitTestResults => {
+	// print out results from unit tests
 	return new Promise(resolve => {
 		platforms.forEach(platform => {
 			const header = `
@@ -39,16 +38,14 @@ ${platform.toUpperCase()}
 		});
 		resolve();
 	});
-});
-
-// run the appium mocha tests
-p = p.then(() => {
+})
+.then(() => {
+	// run the appium mocha tests
 	return new Promise((resolve, reject) => {
 		ui.test(platforms, resolve, reject);
 	});
-});
-
-p.catch(err => {
+})
+.catch(err => {
 	const msg = err.stack || err.toString();
 	console.log(msg);
 	process.exit(1);
